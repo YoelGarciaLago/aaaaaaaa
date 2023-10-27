dac6464dc53c12cbb2efd1dd46db5b98868236e6
Yoel <ygarcialago@danielcastelao.org>
2023-10-27 10:01:56 +0200

    proyecto creado, readme terminado y  jar creado y movido

---
+++ Boletín3_1.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package boletín3_1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author dam1
+ */
+public class Boletín3_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Scanner sc=new Scanner(System.in);
+        float descuento=(float) 0.3;
+        System.out.println("Esto es el descuento"+descuento);
+        System.out.println("Cual es el precio");
+        float precio=sc.nextFloat();
+        float preciofinal=precio*descuento;
+        System.out.println("el precio final es"+ preciofinal);
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+    
+}
--- README.md
+++ README.md
@@ -4,3 +4,32 @@ Para utilizarlo en el examen
 1º Clonar con fork, modificar, añadir el programa, añadir el programa y el readme a git, hacerles commit y luego un push
 
 2º Hacer el jar:
+
+```
+2.1 Abrir intelliJ y crear un proyecto con el programa que quieras hacerle el jar 
+
+2.2 Ir a Proyect Structure en el apartado File del IDE
+
+2.3 Ir al apartado de artifacts, darle al símbolo + , luego a JAR y a Module with dependences
+
+2.4 Escoger el directorio de la clase priincipal y en el menú, antes de darle a aplicar, seleccionas la opción de "copy to the output directory and link via manifest".
+
+2.5 Ir al Build y hay darle a build artifacts
+
+2.6 Comprobar que el .jar funciona y listo 
+
+```
+
+
+Comandos:
+```
+git add 
+
+git commit 
+
+git push 
+
+git remote set-url origin
+
+java -jar Nombrearchivo.jar
+```
